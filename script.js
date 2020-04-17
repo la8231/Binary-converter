22ade93dbbb19008059831dda88d63031063eaeb
Lasse Holm Gundesen <57224603+la8231@users.noreply.github.com>
2020-04-17 11:00:48 +0200

    Start

---
+++ script.js
@@ -0,0 +1,8 @@
+var DecimalInput = document.getElementById('DecimalInput')
+var BinaryInput = document.getElementById('BinaryInput')
+
+var binary = BinaryInput
+
+var digit = parseInt(binary, 2)
+
+console.log(digit)
